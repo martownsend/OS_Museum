b1f95c7827a8a7d37a16603ec5b12bc2f0c4f5e6
mtownsend <townsound@proton.me>
2025-09-09 19:03:38 -0700

    Playing around with quit button

--- osmuseum_1/Scenes/exit_sign_object_1.gd
+++ osmuseum_1/Scenes/exit_sign_object_1.gd
@@ -1,5 +1,5 @@
-extends Node3D
+extends TextureButton
 
 
-func _on_exit_sign_object_1_pressed() -> void:
+func _on_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
