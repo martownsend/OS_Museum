2d0989d0f60f82a9f80b41bb040636457036b0b4
mtownsend <townsound@proton.me>
2025-09-09 18:43:10 -0700

    Implemented basic camera zoom controls (no smoothing)

--- osmuseum_1/Scenes/object_1_camera.gd
+++ osmuseum_1/Scenes/object_1_camera.gd
@@ -1,8 +1,8 @@
 extends Camera3D
 
-signal zoom(in_out: bool)
-@warning_ignore("unused_signal")
-
 func _on_zoom_in_object_1_pressed() -> void:
-	#position.z -= .2 
-	zoom.emit(true)
+	position.z -= .2 
+
+
+func _on_zoom_out_object_1_pressed() -> void:
+	position.z -= -.2
