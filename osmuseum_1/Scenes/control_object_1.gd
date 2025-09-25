a74de57fdbab7fb38e52190c3f99b5572a4fa616
mtownsend <townsound@proton.me>
2025-09-25 12:19:07 -0700

    Esc. button and UI edits

--- osmuseum_1/Scenes/control_object_1.gd
+++ osmuseum_1/Scenes/control_object_1.gd
@@ -1,5 +1,14 @@
 extends Control
 
+func _ready():
+	set_process(true)
 
-func _on_exit_sign_object_1_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+func _process(delta):
+	if Input.is_action_pressed("key_exit"):
+		get_tree().change_scene_to_file("res://main_menu.tscn")
+
+
+#func _input(event):
+	
+	#if Input.is_action_pressed("key_exit"):
+		#get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
--- osmuseum_1/Scenes/exit_sign_object_1.gd
+++ osmuseum_1/Scenes/exit_sign_object_1.gd
@@ -1,1 +1,4 @@
 extends TextureButton
+
+func _on_exit_sign_object_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
