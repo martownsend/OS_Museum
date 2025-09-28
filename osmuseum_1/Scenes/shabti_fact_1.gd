dd159e63acbf15ebed399803dbd7e2dfa975af24
mtownsend <townsound@proton.me>
2025-09-27 19:11:59 -0700

    Testing various button techniques.

--- osmuseum_1/Scenes/shabti_fact_1.gd
+++ osmuseum_1/Scenes/shabti_fact_1.gd
@@ -7,3 +7,7 @@ func _on_close_requested() -> void:
 
 func _on_shabti_button_pressed() -> void:
 	self.visible = !self.visible
+
+
+func _on_area_3d_mouse_entered() -> void:
+	self.visible = !self.visible
---
+++ osmuseum_1/Scenes/shabti_label_test.gd
@@ -0,0 +1,10 @@
+extends Sprite2D
+
+
+
+func _on_shabti_button_pressed() -> void:
+	self.visible = !self.visible
+
+
+func _on_shabti_exit_test_pressed() -> void:
+	self.visible =!self.visible
