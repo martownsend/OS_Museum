386ff1038cd80af205f99893e3c160e788aa2f6e
mtownsend <townsound@proton.me>
2026-01-15 13:06:15 -0800

    no message

--- osmuseum_1/Scenes/object_1_camera.gd
+++ osmuseum_1/Scenes/object_1_camera.gd
@@ -1,8 +1,8 @@
 extends Camera3D
 
 func _on_zoom_in_object_1_pressed() -> void:
-	position.z -= .2 
+	position.z -= .1 
 
 
 func _on_zoom_out_object_1_pressed() -> void:
-	position.z -= -.2
+	position.z -= -.1
